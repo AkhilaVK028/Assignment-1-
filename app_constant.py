1a63f96c914260715fffc351a8e903ae8999ecf2
AkhilaVK028 <132654781+AkhilaVK028@users.noreply.github.com>
2023-07-10 16:42:49 +0530

    Add files via upload

---
+++ app_constant.py
@@ -0,0 +1,10 @@
+class APIs:
+    intro = "/"
+    to_add_data = "/student_registration"
+    to_view_data = "/to_read_data"
+    to_update = "/to_update"
+    to_delete = "/to_delete"
+
+
+class Sql:
+    sql_url = "postgresql://interns:interns%40123@192.168.0.220:5432/internsb2"
